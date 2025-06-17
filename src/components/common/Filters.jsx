dc6e20a8e16dd56e4f6863035823f05582283e69
Yicheng Wang <yichengw1011@gmail.com>
2025-06-17 10:57:29 -0700

    change brand name in filter component

--- src/components/common/Filters.jsx
+++ src/components/common/Filters.jsx
@@ -93,10 +93,11 @@ const Filters = ({ closeModal }) => {
             onChange={onBrandFilterChange}
           >
             <option value="">All Brands</option>
-            <option value="salt">Salt Maalat</option>
-            <option value="betsin">Betsin Maalat</option>
-            <option value="black">Black Kibal</option>
-            <option value="sexbomb">Sexbomb</option>
+            <option value="fortune">Fortune Crystals</option>
+            <option value="midnight">Midnight Aura Jewels</option>
+            <option value="lavender">Lavender Dreams</option>
+            <option value="clarity">Clarity Gems Co</option>
+            <option value="lunaria">Lunaria Crystals</option>
           </select>
         )}
       </div>
